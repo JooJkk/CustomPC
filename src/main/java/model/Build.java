392df05b9279beead8532a347d397a7473297f4a
Lucas Carvalho <lucascarvalhoo@outlook.com>
2026-04-19 13:53:20 -0300

    Update na classe build (Ainda falta implementar o metodo de validação)

--- src/main/java/model/Build.java
+++ src/main/java/model/Build.java
@@ -1,23 +1,79 @@
+package main.java.model;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Build {
     private long id;
     private String nome;
     private int consumoTotalWatts;
-    private Boolean compativel;
+    private boolean compativel;
+    private Processador processador;
+    private PlacaMae placaMae;
+    private Fonte fonte;
+    private List<MemoriaRAM> memorias = new ArrayList<>();
+    private PlacaVideo gpu;
 
-/*
-    private List<Componente> componentes = new ArrayList<>();
-
-    public void adicionarComponente(Componente componente) {
-        componentes.add(componente);
+    public void addMemoria(MemoriaRAM memoria) {
+        memorias.add(memoria);
     }
 
-    public double calcularConsumoTotal() {
+    public void setFonte(Fonte fonte) {
+        this.fonte = fonte;
+    }
+
+    public void setGpu(PlacaVideo gpu) {
+        this.gpu = gpu;
+    }
+
+    public void setMemorias(List<MemoriaRAM> memorias) {
+        this.memorias = memorias;
+    }
+
+    public void setPlacaMae(PlacaMae placaMae) {
+        this.placaMae = placaMae;
+    }
+
+    public void setProcessador(Processador processador) {
+        this.processador = processador;
+    }
+
+    public Fonte getFonte() {
+        return fonte;
+    }
+
+    public List<MemoriaRAM> getMemorias() {
+        return memorias;
+    }
+
+    public PlacaMae getPlacaMae() {
+        return placaMae;
+    }
+
+    public Processador getProcessador() {
+        return processador;
+    }
+
+    public PlacaVideo getGpu() {
+        return gpu;
+    }
+
+    public int calcularConsumoTotal() {
         int total = 0;
-        for (Componente c : componentes) {
-            total = total + c.getConsumoWatts();
+        if (processador != null) {
+            total += processador.getConsumoWatts();
         }
+        if (placaMae != null) {
+            total += placaMae.getConsumoWatts();
+        }
+        if (gpu != null) {
+            total += gpu.getConsumoWatts();
+        }
+
+        for (MemoriaRAM memoria : memorias) {
+            total += memoria.getConsumoWatts();
+        }
+
         this.consumoTotalWatts = total;
         return total;
     }
@@ -31,13 +87,25 @@ public class Build {
 
     public double calcularPrecoTotal() {
         double total = 0.0;
-        for (Componente c : componentes) {
-            total = total + c.getPreco();
+        if (processador != null) {
+            total += processador.getPreco();
         }
+        if (placaMae != null) {
+            total += placaMae.getPreco();
+        }
+        if (fonte != null) {
+            total += fonte.getPreco();
+        }
+        if (gpu != null) {
+            total += gpu.getPreco();
+        }
+
+        for (MemoriaRAM memoria : memorias) {
+            total += memoria.getPreco();
+        }
+
         return total;
     }
-
- */
 
 
     public void setId (long id) {
@@ -61,20 +129,15 @@ public class Build {
         return consumoTotalWatts;
     }
 
-    public void setCompativel(Boolean compativel){
+    public void setCompativel(boolean compativel){
         this.compativel = compativel;
     }
-    public Boolean getCompativel() {
+    public boolean isCompativel() {
         return compativel;
     }
 
     public Build() {
 
     }
-
-
-
-
-
 
 }
