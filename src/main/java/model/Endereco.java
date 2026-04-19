4509cc81790b70094eb1696395f3e96986e2c923
Lucas Carvalho <lucascarvalhoo@outlook.com>
2026-04-19 16:38:42 -0300

    Adiciona classe endereço (implementada por SamequeK)

---
+++ src/main/java/model/Endereco.java
@@ -0,0 +1,61 @@
+package main.java.model;
+
+public class Endereco {
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String cep;
+    private String estado;
+
+
+    public Endereco(String rua, String numero, String barrio, String cidade, String cep, String estado) {
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = barrio;
+        this.cidade = cidade;
+        this.cep = cep;
+        this.estado = estado;
+    }
+
+
+
+    public String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {this.rua = rua;}
+
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero (String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;}
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {this.estado = estado;
+    }
+
+}
