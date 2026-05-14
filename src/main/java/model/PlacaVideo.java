bfaa84bde5fe38521d1c09798cf0715e8c83a6fa
Lucas Carvalho <lucascarvalhoo@outlook.com>
2026-05-14 11:36:18 -0300

    validações na placa de video

--- src/main/java/model/PlacaVideo.java
+++ src/main/java/model/PlacaVideo.java
@@ -3,24 +3,45 @@ package main.java.model;
 public class PlacaVideo extends Componente{
     private int comprimentoMM;
     private int memoriaGB;
-    public PlacaVideo(String nome, String marca, double preco, double peso, int estoque, int consumoWatts, int comprimentoMM, int memoriaGB){
+    public PlacaVideo(String nome, String marca, double preco, double peso, int estoque, int consumoWatts, int comprimentoMM, int memoriaGB) {
+
         super(nome, marca, preco, peso, estoque, consumoWatts);
-        this.comprimentoMM = comprimentoMM;
-        this.memoriaGB = memoriaGB;
+
+        setComprimentoMM(comprimentoMM);
+        setMemoriaGB(memoriaGB);
     }
-    public PlacaVideo(){
+
+    public PlacaVideo() {
         super();
     }
+
     public int getComprimentoMM() {
         return comprimentoMM;
     }
+
     public void setComprimentoMM(int comprimentoMM) {
+
+        if(comprimentoMM <= 0){
+            throw new IllegalArgumentException(
+                    "comprimento deve ser maior que zero"
+            );
+        }
+
         this.comprimentoMM = comprimentoMM;
     }
+
     public int getMemoriaGB() {
         return memoriaGB;
     }
+
     public void setMemoriaGB(int memoriaGB) {
+
+        if(memoriaGB <= 0){
+            throw new IllegalArgumentException(
+                    "memória deve ser maior que zero"
+            );
+        }
+
         this.memoriaGB = memoriaGB;
     }
 }
